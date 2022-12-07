59509b00452a238eaf12a02419bb83a9ad2775ec
Pawandeep962 <118867088+Pawandeep962@users.noreply.github.com>
2022-12-07 11:25:54 +1100

    Add files via upload

---
+++ Customer_index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" type="image/x-icon" href="img/logo1.png" />
+  <title>Checked and Balanced</title>
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.2.0/ekko-lightbox.css" />
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body id="home">
+<nav class="navbar navbar-expand-sm fixed-top ">
+  <div class="container">
+
+
+    <a href="#" class=" navbar-toggler" data-toggle="collapse" data-target="#navCollapse">
+      <i class="fa fa-sort-down"></i>
+      </a>
+  <div class="collapse navbar-collapse text-center" id="navCollapse">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active px-3"><a class="nav-link" href="#home">
+      Products
+      </a></li>
+      <li class="nav-item px-3"><a class="nav-link" href="#service-head">Sets</a></li>
+      <li class="nav-item px-3"><a class="nav-link" href="#contact">Reveiws</a></li>
+    </ul>
+  </div>
+  <a href="index.html">
+    <img src="img/Capture.jpg " width="170px" class="Logo">
+  </a>
+
+  </div>
+</nav>
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.2.0/ekko-lightbox.js"></script>
+  <script src="js/main.js"></script>
+</body>
+</html>
